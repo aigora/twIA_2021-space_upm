43df5523115c288e228e84d1ccac136cf76cf64c
Pigamer37 <lucas.gomez.velayos@alumnos.upm.es>
2021-05-05 21:48:46 +0200

    Cambio de delay por millis y limpieza de código
    He cambiado delay() por millis() ya que las dos nos ayudan a que Arduino  haga una pausa, pero delay() para todo el programa, mientras que millis() simplemente mide el tiempo; por lo que si le decimos que se ejecute cada vez que pase una diferencia de tiempo especificada, realizamos el mismo propósito pero sin detener el programa.
    
    Aún así he mantenido los delay de la función de ultrasonidos, ya que pausa el programa durante microsegundos. Si detectáramos que da problemas, lo podemos cambiar también.

--- Codigo_arduino_ultrasunidos_y_servo/Codigo_arduino_ultrasunidos_y_servo.ino
+++ Codigo_arduino_ultrasunidos_y_servo/Codigo_arduino_ultrasunidos_y_servo.ino
@@ -1,13 +1,14 @@
 #include <Servo.h>
 Servo upm;
+
 #define trig 7
 #define echo 6
+
 int X = 0;
+int T_bucle = 100;
 long distancia;
-
 long medirDistancia();
-
-
+unsigned long Tiempo = millis();
 
 void setup() {
 Serial.begin(9600);
@@ -18,23 +19,18 @@ while(!Serial) { ; }
 }
 
 void loop() {
-distancia=medirDistancia();
-Serial.print ("distancia = ");
-Serial.println(distancia);
-int position_X = analogRead(X);
-int angulo = map(position_X, 0, 1023, 0, 180);
-Serial.print ("angulo = ");
-Serial.println(angulo);
-upm.write(angulo);
-delay(100);
-
-
-
+  
+  if(millis()-Tiempo>=T_bucle){
+    distancia=medirDistancia();
+    int position_X = analogRead(X);
+    int angulo = map(position_X, 0, 1023, 0, 180);
+    Serial.println(distancia);
+    Serial.println(angulo);
+    upm.write(angulo);
+  }
 }
 long medirDistancia() {
 long duration, distanceCm; //duration es long porque pulseIN nos devuelve el tiempo en microsec.
-
-
 
 digitalWrite(trig, LOW); //por cuestion de estabilizacion del sensor
 delayMicroseconds(4);
@@ -42,13 +38,9 @@ digitalWrite(trig, HIGH); //generamos Trigger (disparo) de 10us
 delayMicroseconds(10);//Tiene que estar 10us en ON
 digitalWrite(trig, LOW);
 
-
-
 duration = pulseIn(echo, HIGH); //medimos el tiempo hasta que el pulso vuelve a llegar, en microsegundos con la funcion Pulseln(), calculando el tiempo que esta ECHO en HIGH
 //mientras recibe el ultrasonidos.
 
-
-
-distanceCm = duration /(29.1*2); //convertimos la distancia en cm. No olvidemos dividirlo con 2 tambien.(1s/340m=0.00291s/m=)
+distanceCm = duration /(29.1*2); //convertimos la distancia en cm. No olvidemos dividirlo entre 2 también.(1s/340m=0.00291s/m)
 return distanceCm;
 }
