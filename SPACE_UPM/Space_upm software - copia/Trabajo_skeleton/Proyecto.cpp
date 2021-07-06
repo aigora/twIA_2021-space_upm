4fdffbb7760badf9f6a2e38681ffc52a2af292f3
Pigamer37 <lucas.gomez.velayos@alumnos.upm.es>
2021-07-06 21:25:10 +0200

    Lógica de fichero

--- SPACE_UPM/Space_upm software - copia/Trabajo_skeleton/Proyecto.cpp
+++ SPACE_UPM/Space_upm software - copia/Trabajo_skeleton/Proyecto.cpp
@@ -17,25 +17,48 @@ int main()
 	int i, j;
 	char matriz[y_size][x_size]; //declaración de matriz, pudiendo cambiar su tamaño (PENDIENTE DE IMPLEMENTACIÓN EN ARDUINO)
 
-	for (i = 0; i < y_size; i++) { //rellenamos matriz con 0 (vacío)
-		for (j = 0; j < x_size; j++) {
-			matriz[i][j] = '0';
+	FILE* ar;
+	errno_t err;
+	err = fopen_s(&ar, "plano.txt", "w+");
+	if (err == 0) {
+
+		for (i = 0; i < y_size; i++) { //rellenamos matriz con 0 (vacío)
+			for (j = 0; j < x_size; j++) {
+				matriz[i][j] = '0';
+			}
 		}
-	}
 
-	while (Arduino->IsConnected())
-	{
 
-		bytesRecibidos = Arduino->ReadData(BufferEntrada, sizeof(char) * Tamano_com);
-
-		if (bytesRecibidos != -1) // Lectura de mensaje desde el puerto
+		while (Arduino->IsConnected())
 		{
-			BufferEntrada[bytesRecibidos-1] = '\0';
-			printf("Recibidos %d bytes: %s\n", bytesRecibidos, BufferEntrada);
+
+			bytesRecibidos = Arduino->ReadData(BufferEntrada, sizeof(char) * Tamano_com);
+
+			if (bytesRecibidos != -1) // Lectura de mensaje desde el puerto
+			{
+				BufferEntrada[bytesRecibidos - 1] = '\0';
+				printf("Recibidos %d bytes: %s\n", bytesRecibidos, BufferEntrada);
+			}
+			else {
+				printf("No se ha recibido nada\n");
+			}
+			Sleep(2000);
 		}
-		else {
-			printf("No se ha recibido nada\n");
+
+		for (i = 0; i < y_size; i++) { //ponemos la matriz en el archivo
+			for (j = 0; j < x_size; j++) {
+				fprintf_s(ar, "%c", matriz[i][j]);
+			}
+			fprintf_s(ar, "\n");
 		}
-		Sleep(2000);
+
+
+		fclose(ar);
+		if (fclose(ar) == NULL) 
+			printf("Archivo cerrado con exito");
+		else
+			printf("Error al cerrar el archivo");
 	}
+	else
+		printf("Archivo no abierto correctamente");
 }
